3b3f84ccc01d6f65260caf6c7053ae479d55cd23
siamparvez001 <siamparvez003@gmail.com>
2026-06-23 06:26:47 +0600

    fix auth-client

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || "https://vitaflow-client-183xa23wa-siamparvez003-5195s-projects.vercel.app/"
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://vitaflow-server.vercel.app"
 })
 export const { signIn, signUp, signOut ,useSession } = authClient;
